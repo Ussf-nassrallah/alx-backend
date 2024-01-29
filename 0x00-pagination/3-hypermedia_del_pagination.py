ac7e6a540606dc0ae9eac58a2570fd55616a1523
Ussf-nassrallah <nassrallah.dev@gmail.com>
2024-01-29 11:03:18 +0100

    done: hypermedia_del_pagination

---
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+"""
+Deletion-resilient hypermedia pagination
+"""
+
+import csv
+import math
+from typing import List, Dict
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0
+        """
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """Retrieves info about a page from a given index and with a
+        specified size.
+        """
+        data = self.indexed_dataset()
+        assert index is not None and index >= 0 and index <= max(data.keys())
+
+        page_content = []
+        count = 0
+        next_idx = None
+        start_idx = index if index else 0
+
+        for key, value in data.items():
+            if key >= start_idx and count < page_size:
+                page_content.append(value)
+                count += 1
+                continue
+            if count is page_size:
+                next_idx = key
+                break
+
+        output: Dict = {
+            'index': index,
+            'next_index': next_idx,
+            'page_size': len(page_content),
+            'data': page_content,
+        }
+
+        return output
