f92c163b97fc40c7ed7d0d55fa8b8be003ed07d5
Ussf-nassrallah <nassrallah.dev@gmail.com>
2024-01-31 10:29:20 +0100

    mru_cache

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env pthon3
+''' MRU Caching '''
+
+from collections import OrderedDict
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """ MRUCache """
+    def __init__(self):
+        ''' constructor '''
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        ''' implement put method '''
+        data = self.cache_data
+
+        if key is None or item is None:
+            return
+
+        if key not in data:
+            if len(data) + 1 > self.MAX_ITEMS:
+                target_key, _ = data.popitem(False)
+                print("DISCARD:", target_key)
+
+            data[key] = item
+            data.move_to_end(key, last=False)
+        else:
+            data[key] = item
+
+    def get(self, key):
+        ''' implement get method '''
+        data = self.cache_data
+
+        if key is None or key not in data:
+            return None
+
+        if key is not None and key in data:
+            data.move_to_end(key, last=False)
+
+        return data[key]
