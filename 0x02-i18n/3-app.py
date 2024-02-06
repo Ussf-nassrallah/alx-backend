35723924b0168287aa66bd49598f6cd362a8adaf
Ussf-nassrallah <nassrallah.dev@gmail.com>
2024-02-06 11:48:39 +0100

    task-3

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+''' flask app '''
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+
+class Config:
+    ''' Babel conf '''
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    ''' render 2-index.html page in '/' endpoint '''
+    return render_template('3-index.html')
+
+
+@babel.localeselector
+def get_locale() -> str:
+    ''' Get locale from request '''
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+if __name__ == "__main__":
+    app.run(port=5000)
