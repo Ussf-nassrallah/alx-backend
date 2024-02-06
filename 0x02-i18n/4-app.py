2a351047ea607cc33333ec16946ebe8e2b270c7b
Ussf-nassrallah <nassrallah.dev@gmail.com>
2024-02-06 12:20:04 +0100

    task-4 fix

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -20,24 +20,26 @@ babel = Babel(app)
 
 @app.route('/', strict_slashes=False)
 def index() -> str:
-    ''' render 3-index.html page in '/' endpoint '''
-    return render_template('3-index.html')
+    ''' render 4-index.html page in '/' endpoint '''
+    return render_template('4-index.html')
 
 
 @babel.localeselector
 def get_locale() -> str:
     ''' Get locale from request '''
     query_params = request.query_string.decode('utf-8').split('&')
+
     query_params_dict = dict(map(
-        lambda param: (param if '=' in param else '{}='.format(param)).split('='),
+        lambda param: (
+            param if '=' in param else '{}='.format(param)
+        ).split('='),
         query_params,
     ))
-    
-    if 'user_locale' in query_params_dict:
-        user_locale = query_params_dict['user_locale']
-        if user_locale in app.config["LANGUAGES"]:
-            return user_locale
-    
+
+    if 'locale' in query_params_dict:
+        if query_params_dict['locale'] in app.config["LANGUAGES"]:
+            return query_params_dict['locale']
+
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
